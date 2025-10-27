159ebc2322a99f572d6f45ce8460bf54bfeb9706
Abhi Passi <abhijit36254@gmail.com>
2025-10-27 15:11:46 +0530

    admission form

--- backend/controllers/studentControllers.js
+++ backend/controllers/studentControllers.js
@@ -3,6 +3,71 @@ export const demo = (req, res) => {
     res.json("Student Routes Working")
 }
 
-// export const admission = (req,res) =>{
-    
-// }
+export const uploadFile = (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: "No file uploaded!" });
+        }
+
+        // You can access req.file for file info, and save file data to DB if needed
+        res.status(200).json({
+            message: "File uploaded successfully!",
+            file: req.file
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const newAdmission = async (req, res) => {
+    try {
+        const {
+            courseApplied,
+            studentName,
+            fatherName,
+            motherName,
+            dob,
+            address,
+            city,
+            state,
+            pinCode,
+            schoolName,
+            schoolCity,
+            fatherOccupation,
+            fatherAddress,
+            studentEmail,
+            parentContact,
+            studentContact,
+        } = req.body;
+
+        
+        const image = req.file ? req.file.filename : null;
+
+        const response = await admission.create({
+            courseApplied,
+            studentName,
+            fatherName,
+            motherName,
+            dob,
+            address,
+            city,
+            state,
+            pinCode,
+            schoolName,
+            schoolCity,
+            fatherOccupation,
+            fatherAddress,
+            studentEmail,
+            parentContact,
+            studentContact,
+            image, 
+        });
+
+        res.status(201).json({ success: true, response });
+        console.log("New admission added:", response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+};
--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 const router = express.Router()
+import upload from '../middleware/multerConfig.js'
 import {
-    demo
+    demo,
+    uploadFile,
+    newAdmission
 } from "../controllers/studentControllers.js"
 
 router.get('/demo', demo)
+
+router.post('/admission', upload.single("image"),newAdmission)
 
 export default router
 
